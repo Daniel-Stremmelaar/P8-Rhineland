e8c16b2543e6eb529a6c7f705e08b765284694d7
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-04-17 15:37:59 +0200

    cam move
    pls no bugs

---
+++ Unity Project/P8 Rhineland/Assets/Scripts/CamraMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamraMove : MonoBehaviour
+{
+    public float camMoveSpeed;
+    public float screenThickness;
+
+    void Update()
+    {
+        CameraMove();
+    }
+
+    //cam movement
+    void CameraMove()
+    {
+        Vector3 currPos = transform.position;
+
+        //forward
+        if (Input.GetAxis("Vertical") > 0 || Input.mousePosition.y >= Screen.height - screenThickness)
+        {
+            currPos.z -= camMoveSpeed * Time.deltaTime;
+        }
+        //back
+        if (Input.GetAxis("Vertical") < 0 || Input.mousePosition.y <= screenThickness)
+        {
+            currPos.z += camMoveSpeed * Time.deltaTime;
+        }
+        if (Input.GetAxis("Horizontal") > 0 || Input.mousePosition.x >= Screen.width - screenThickness)
+        //right
+        {
+            currPos.x -= camMoveSpeed * Time.deltaTime;
+        }
+        //left
+        if (Input.GetAxis("Horizontal") < 0 || Input.mousePosition.x <= screenThickness)
+        {
+            currPos.x += camMoveSpeed * Time.deltaTime;
+        }
+
+        transform.position = currPos;
+    }
+
+    //scrool wheel zoom in and out
+}
