62f3ff4ab110e2f53f32b442510b39b14ab93422
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-05-04 18:08:42 +0200

    start RandomEvents
    just a start tho

---
+++ Unity Project/P8 Rhineland/Assets/Scripts/RandomEvent/RandomEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEvent : MonoBehaviour
+{
+    public List<RandomEventPlus> randomEventPlus = new List<RandomEventPlus>();
+    public List<RandomEventMinus> randomEventMinus = new List<RandomEventMinus>();
+    
+}
+
+[System.Serializable]
+public class RandomEventPlus
+{
+    public string name;
+    public string discription;
+
+    public int plusSomthing;
+}
+
+[System.Serializable]
+public class RandomEventMinus
+{
+    public string name;
+    public string discription;
+
+    public int minusSomthing;
+}
---
+++ Unity Project/P8 Rhineland/Assets/Scripts/RandomEvent/RandomEventsManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//select an event
+public class RandomEventsManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
