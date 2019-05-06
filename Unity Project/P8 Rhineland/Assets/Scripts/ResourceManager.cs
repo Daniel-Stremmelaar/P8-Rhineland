60e963a8c33a8c58950cb44524ca268bfe18cab6
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-05-06 13:56:54 +0200

    ui and base code
    furter resources

--- Unity Project/P8 Rhineland/Assets/Scripts/ResourceManager.cs
+++ Unity Project/P8 Rhineland/Assets/Scripts/ResourceManager.cs
@@ -7,18 +7,31 @@ public class ResourceManager : MonoBehaviour
 {
     public int wood;
     public int stone;
+    public int food;
+    public int gold;
+    public int happiness;
+    public int popualtion;
+    public int popCap;
+
     public Text woodText;
     public Text stoneText;
+    public Text foodText;
+    public Text goldText;
+    public Text happinessText;
+    public Text popualtionText;
 
     private void Start()
     {
-        woodText.text = "Wood: " + wood.ToString();
-        stoneText.text = "Stone: " + stone.ToString();
+        UpdateUI();
     }
 
     public void UpdateUI()
     {
         woodText.text = "Wood: " + wood.ToString();
         stoneText.text = "Stone: " + stone.ToString();
+        foodText.text = "Food: " + food.ToString();
+        goldText.text = "Gold: " + gold.ToString();
+        happinessText.text = "Happiness: " + happiness.ToString();
+        popualtionText.text = "Popualtion: " + popualtion.ToString() + "/" + popCap.ToString();
     }
 }
