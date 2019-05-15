3fb4f4784ac1cc6ffc7db622c58b74b35edf14a7
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2019-05-15 14:04:56 +0200

    Gatherer update
    Gatherer can now use gain function again

--- Unity Project/P8 Rhineland/Assets/Scripts/ScriptableObjects/GathererType.cs
+++ Unity Project/P8 Rhineland/Assets/Scripts/ScriptableObjects/GathererType.cs
@@ -12,16 +12,41 @@ public class GathererType : ScriptableObject
 
     [Header("Data")]
     public int goldCost;
+    private int type;
 
-    public enum resources { Wood, Stone };
+    public enum resources { Wood, Stone, Planks, Berry, IronOre, GoldOre };
     public resources resource;
 
     public void Deliver(int i, GameObject g)
     {
+        if(resource == resources.Wood)
+        {
+            type = 0;
+        }
+        if(resource == resources.Stone)
+        {
+            type = 2;
+        }
+        if(resource == resources.Planks)
+        {
+            type = 1;
+        }
+        if(resource == resources.Berry)
+        {
+            type = 7;
+        }
+        if(resource == resources.IronOre)
+        {
+            type = 10;
+        }
+        if(resource == resources.GoldOre)
+        {
+            type = 11;
+        }
         if (i > 0)
         {
             Debug.Log("Delivered " + i.ToString() + " " + resource.ToString());
-            g.GetComponent<Building>().r.Gain(g.GetComponent<Building>().type.recieveType.ToString(), i);
+            g.GetComponent<Building>().r.Gain(type, i);
         }
     }
 }
