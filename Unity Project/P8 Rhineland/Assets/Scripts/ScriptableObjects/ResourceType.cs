c116a6df068a371d337c8117f33ce60e9b865003
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-05-15 13:08:51 +0200

    Data resource
    source

---
+++ Unity Project/P8 Rhineland/Assets/Scripts/ScriptableObjects/ResourceType.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Resource", menuName = "Resource Data", order = 51)]
+public class ResourceType : ScriptableObject
+{
+    string resourceName;
+
+    public int quantity;
+
+    public List<GameObject> required = new List<GameObject>();
+}
