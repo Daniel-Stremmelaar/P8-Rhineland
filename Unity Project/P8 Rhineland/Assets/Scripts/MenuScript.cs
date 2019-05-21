fc51e4070aa01a024ac8491d29b02ef7f1acf497
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-05-21 14:12:59 +0200

    start met het stratMenu
    menu start

---
+++ Unity Project/P8 Rhineland/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject startPanel;
+    public GameObject optionsPanel;
+
+    void Awake()
+    {
+        optionsPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ButtonStart()
+    {
+        SceneManager.LoadScene(1);
+        
+    }
+
+    public void ButtonOptions()
+    {
+        startPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void ButtonExit()
+    {
+        Application.Quit();
+        Debug.Log("GAME HAS STOPPED");
+    }
+
+    public void ButtonBackToStart()
+    {
+        startPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+    }
+
+    //sound 
+    //screen whith and hight
+    //
+}
