e05e3eb694722f680f8e0f5ba9d3fb9347a67b48
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-05-27 15:34:04 +0200

    start random map
    start random map

---
+++ Unity Project/P8 Rhineland/Assets/Scripts/WorldGeneration/GenerateWorld.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateWorld : MonoBehaviour
+{
+    #region All Variables
+    [Header("All Amount")]
+    [SerializeField] int maxAmountTrees;
+    [SerializeField] int maxAmountSmallRocks;
+    [SerializeField] int maxAmountBigRocks;
+    [SerializeField] int maxAmountBush;
+    [SerializeField] int minTrees;
+    [SerializeField] int minBigRocks;
+    [SerializeField] int minSmallRocks;
+    [SerializeField] int minBush;
+
+    [Header("All Objecten")]
+    [SerializeField] Transform tree;
+    [SerializeField] Transform smallrocks;
+    [SerializeField] Transform bigrocks;
+    [SerializeField] Transform bush;
+
+    [SerializeField] float offset;
+    [SerializeField] LayerMask mask;
+    Transform newrock;
+    [Header("RockSize")]
+    [SerializeField] float minsize;
+    [SerializeField] float maxsize;
+    [Header("All Lists")]
+    public List<Transform> allTrees = new List<Transform>();
+    public List<Transform> allSmallRocks = new List<Transform>();
+    public List<Transform> allBigRocks = new List<Transform>();
+    public List<Transform> allBush = new List<Transform>();
+    [Header("Timer")]
+    [SerializeField] float mintimer;
+    [SerializeField] float maxtimer;
+    public float thinkTimer;
+    bool addTree;
+    bool addbRock;
+    bool addsRock;
+    bool addBush;
+    RaycastHit hit;
+    float newsize;
+    float newyPos;
+    float newzPos;
+    float newxPos;
+    float xSize;
+    float zSize;
+    Vector3 newWorldpos;
+    #endregion
+
+    void Start()
+    {
+        xSize = GetComponent<MeshRenderer>().bounds.size.x / 2;
+        zSize = GetComponent<MeshRenderer>().bounds.size.z / 2;
+        newWorldpos = Vector3.zero;
+        Generate();
+    }
+    void Generate()
+    {
+        #region SpawnTrees
+        for (int i = 0; i < maxAmountTrees; i++)
+        {
+            newxPos = Random.Range(-xSize, xSize);
+            newzPos = Random.Range(-zSize, zSize);
+            if (Physics.Raycast(new Vector3(newxPos, 9999f, newzPos), Vector3.down, out hit, Mathf.Infinity, mask))
+            {
+                if (hit.transform.tag == "Ground")
+                {
+                    newyPos = hit.point.y;
+                    newWorldpos = new Vector3(newxPos, newyPos, newzPos);
+                    Transform newTree = Instantiate(tree, newWorldpos, Quaternion.identity);
+                    allTrees.Add(newTree);
+                }
+            }
+        }
+        #endregion
+        #region Bush
+        for (int i = 0; i < maxAmountBush; i++)
+        {
+            newxPos = Random.Range(-xSize, xSize);
+            newzPos = Random.Range(-zSize, zSize);
+            if (Physics.Raycast(new Vector3(newxPos, 9999f, newzPos), Vector3.down, out hit, Mathf.Infinity, mask))
+            {
+                if (hit.transform.tag == "Ground")
+                {
+                    newyPos = hit.point.y;
+                    newWorldpos = new Vector3(newxPos, newyPos, newzPos);
+                    //Transform angleBush = new Transform(newWorldpos, Vector3.Angle(hit.normal));
+                    Transform newBush = Instantiate(bush, newWorldpos, Quaternion.identity);
+                    allBush.Add(newBush);
+                }
+            }
+        }
+        #region SpawnBigRocks
+        for (int i = 0; i < maxAmountBush; i++)
+        {
+            newxPos = Random.Range(-xSize, xSize);
+            newzPos = Random.Range(-zSize, zSize);
+            if (Physics.Raycast(new Vector3(newxPos, 9999f, newzPos), Vector3.down, out hit, Mathf.Infinity, mask))
+            {
+                if (hit.transform.tag == "Ground")
+                {
+                    newyPos = hit.point.y;
+                    newWorldpos = new Vector3(newxPos, newyPos + offset, newzPos);
+                    Transform newBigRock = Instantiate(bush, newWorldpos, Quaternion.identity);
+                    allBigRocks.Add(newBigRock);
+                }
+            }
+        }
+        #endregion
+        #region SpawnSmallRocks
+        for (int i = 0; i < maxAmountSmallRocks; i++)
+        {
+            newxPos = Random.Range(-xSize, xSize);
+            newzPos = Random.Range(-zSize, zSize);
+            var randomrRot = Random.Range(0, 360);
+            newsize = Random.Range(minsize, maxsize);
+            if (Physics.Raycast(new Vector3(newxPos, 9999f, newzPos), Vector3.down, out hit, Mathf.Infinity, mask))
+            {
+                if (hit.transform.tag == "Ground")
+                {
+                    newyPos = hit.point.y;
+                    newWorldpos = new Vector3(newxPos, newyPos + offset, newzPos);
+                    newrock = Instantiate(smallrocks, newWorldpos, Quaternion.Euler(randomrRot, randomrRot, randomrRot));
+                    newrock.localScale = new Vector3(newsize, newsize, newsize);
+                    allSmallRocks.Add(newrock);
+                }
+            }
+        }
+        #endregion
+    }
+    #endregion
+
+    void Update()
+    {
+        CheckAmount();
+        ThinkTimer();
+    }
+
+    void CheckAmount()
+    {
+        if (allTrees.Count < minTrees)
+        {
+            addTree = true;
+        }
+    }
+
+    void ThinkTimer()
+    {
+        if (!addTree)
+        {
+            thinkTimer -= Time.deltaTime;
+            if (thinkTimer <= 0)
+            {
+                thinkTimer = Random.Range(mintimer, maxtimer);
+                //RandomPos();
+            }
+        }
+    }
+
+    void AddItem()
+    {
+
+    }
+}
---
+++ Unity Project/P8 Rhineland/Assets/Scripts/WorldGeneration/WorldGeneration.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGeneration : MonoBehaviour
+{
+    [Header("Nodes")]
+    public GameObject woodNode;
+    public GameObject stoneNode;
+
+    [Header("Amount")]
+    public int woodNodeAmount;
+    public int stoneAmount;
+    public LayerMask mask;
+    RaycastHit hit;
+    Vector3 newWorldPos;
+
+    float xSize;
+    float zSize;
+    float xPos;
+    float zPos;
+    float yPos;
+
+    void Start()
+    {
+        xSize = GetComponent<Terrain>().terrainData.size.x;
+        zSize = GetComponent<Terrain>().terrainData.size.z;
+        Generate();
+    }
+
+    void Generate()
+    {
+        //spawn threes
+        for (int i = 0; i < woodNodeAmount; i++)
+        {
+            xPos = Random.Range(0, xSize);
+            zPos = Random.Range(0, zSize);
+            if (Physics.Raycast(new Vector3(xPos,500f,zPos),Vector3.down,out hit,Mathf.Infinity,mask))
+            {
+                if (hit.transform.tag == "Terrain")
+                {
+                    yPos = hit.point.y;
+                    newWorldPos = new Vector3(xPos, yPos, zPos);
+                   
+                    Instantiate(woodNode, newWorldPos, Quaternion.identity);
+                }
+            }
+        }
+        //spawn stone
+        for (int i = 0; i < stoneAmount; i++)
+        {
+            xPos = Random.Range(0, xSize);
+            zPos = Random.Range(0, zSize);
+            if (Physics.Raycast(new Vector3(xPos,500f,zPos),Vector3.down,out hit,Mathf.Infinity,mask))
+            {
+                if (hit.transform.tag == "Terrain")
+                {
+                    yPos = hit.point.y;
+                    newWorldPos = new Vector3(xPos, yPos, zPos);
+                   
+                    Instantiate(stoneNode, newWorldPos, Quaternion.identity);
+                }
+            }
+        }
+    }
+}
